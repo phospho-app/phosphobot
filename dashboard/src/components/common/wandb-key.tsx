f5d977e556f1309c90da4c298340ea58763b0e16
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2025-07-07 18:42:47 +0200

    Fix merge conflicts

--- dashboard/src/components/common/wandb-key.tsx
+++ dashboard/src/components/common/wandb-key.tsx
@@ -2,7 +2,6 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { fetchWithBaseUrl } from "@/lib/utils";
 import {
   Tooltip,
   TooltipContent,
